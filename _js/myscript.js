886ddab1d1a5e5287ebb42d93a75634bb6f0d2dc
spencer4908 <spencer4908@live.com>
2017-01-06 22:04:24 -0700

    Commented line of code in js to prevent console error

--- _js/myscript.js
+++ _js/myscript.js
@@ -1,4 +1,4 @@
-document.getElementById("changeMe").innerHTML += "<b>Test</b>";
+// document.getElementById("changeMe").innerHTML += "<b>Test</b>";
 // document.getElementById("changeMe").style.color = myColor();
 
 var hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
